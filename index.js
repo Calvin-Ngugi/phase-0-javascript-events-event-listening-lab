a6644a18f84f582d633415b974b30014dc2f33c8
Calvin <ncalvin67@gmail.com>
2022-08-08 10:21:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+//let x = document.getElementById("button");
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('Still don\'t know why it works');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
